045f1e21eaec9a53c333d896af834241f95d9154
Bryan Fordney <126733+bryab@users.noreply.github.com>
2024-02-12 12:52:35 -0500

    support 16-bit audio

--- src/util/sound.rs
+++ src/util/sound.rs
@@ -4,7 +4,7 @@ use std::io::Cursor;
 use wav;
 
 /// Generate a Godot AudioStreamWav from the given 8-bit WAV data
-pub fn sound_from_bytes(data: Vec<u8>) -> Result<Gd<AudioStreamWav>, &'static str> {
+pub fn sound_from_bytes(data: Vec<u8>) -> Result<Gd<AudioStreamWav>, String> {
     let mut file = Cursor::new(data);
     let (header, data) = wav::read(&mut file).map_err(|_| "Invalid WAV data!")?;
     match data {
@@ -18,12 +18,15 @@ pub fn sound_from_bytes(data: Vec<u8>) -> Result<Gd<AudioStreamWav>, &'static st
             wav.set_data(PackedByteArray::from(&d[..]));
             wav.set_mix_rate(header.sampling_rate as i32);
             Ok(wav)
+        },
+        wav::BitDepth::Sixteen(d) => {
+            let mut wav = AudioStreamWav::new_gd();
+            let d: Vec<u8> = d.iter().flat_map(|b|b.to_le_bytes()).collect();
+            wav.set_format(audio_stream_wav::Format::FORMAT_16_BITS);
+            wav.set_data(PackedByteArray::from(&d[..]));
+            wav.set_mix_rate(header.sampling_rate as i32);
+            Ok(wav)
         }
-        // FIXME: I am not sure how - if it is possible - to load 16-bit data into Godot WAV
-        // wav::BitDepth::Sixteen(d) => {
-        //     wav.set_format(audio_stream_wav::Format::FORMAT_16_BITS);
-        //     wav.set_data(PackedByteArray::from(&d[..]));
-        // }
-        _ => Err("Unsupported WAV format"),
+        _ => Err(format!("Unsupported WAV format: {}", header.audio_format)),
     }
 }
